5c35b3cb9ae09497a438e27598e1b60f7658d2f8
Joshua Williams <joshua.jwilliams@gmail.com>
2022-08-22 12:56:23 -0400

    clean up environments to standard output.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
   production: true,
-  API_REST_URL: 'http://localhost',
-  API_REST_PORT: 4000,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,6 @@
 
 export const environment = {
   production: false,
-  API_REST_URL: 'http://localhost',
-  API_REST_PORT: 4200,
 };
 
 /*
